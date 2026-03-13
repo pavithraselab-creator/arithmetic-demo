8003b61b698dc7f44ab3ed1d42f75b8af560a85f
pavithraselab-creator <pavithraselab@gmail.com>
2026-03-13 15:11:32 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,10 @@
+class arithmetic demo
+{
+   public static void main(String args[])
+   {
+      int a=10;
+      int b=40;
+      int sum=a+b;
+      system.out.println("sum of two number:");
+  }
+}
